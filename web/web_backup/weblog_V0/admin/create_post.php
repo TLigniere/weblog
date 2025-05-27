714acae8f89e0ccd5b5dbc87c416de536efb0ac8
TLigniere <timothee.ligniere@gmail.com>
2025-05-27 09:07:22 +0200

    Fix submodule

---
+++ web/web_backup/weblog_V0/admin/create_post.php
@@ -0,0 +1,55 @@
+<?php include('../config.php'); ?>
+<?php include(ROOT_PATH . '/includes/admin_functions.php'); ?>
+<?php include(ROOT_PATH . '/admin/post_functions.php'); ?>
+<?php include(ROOT_PATH . '/includes/admin/head_section.php'); ?>
+
+<?php
+	$topics = getAllTopics();	
+?>
+
+<title>Admin | Create Post</title>
+</head>
+<body>
+	<?php include(ROOT_PATH . '/includes/admin/header.php') ?>
+	<div class="container content">
+		<?php include(ROOT_PATH . '/includes/admin/menu.php') ?>
+
+		<div class="action create-post-div">
+			<h1 class="page-title">Create/Edit Post</h1>
+
+			<form method="post" enctype="multipart/form-data" action="create_post.php">
+				<?php include(ROOT_PATH . '/includes/public/errors.php') ?>
+
+				<?php if ($isEditingPost === true): ?>
+					<input type="hidden" name="post_id" value="<?php echo $post_id; ?>">
+				<?php endif ?>
+
+				<input type="text" name="title" value="<?php echo $title; ?>" placeholder="Title" required>
+
+				<label>Featured image</label>
+				<input type="file" name="featured_image">
+
+				<textarea name="body" id="body"><?php echo $body; ?></textarea>
+
+				<select name="topic_id" required>
+					<option value="" disabled <?php echo empty($topic_id) ? 'selected' : ''; ?>>Choose topic</option>
+					<?php foreach ($topics as $topic): ?>
+						<option value="<?php echo $topic['id']; ?>" <?php if ($topic_id == $topic['id']) echo 'selected'; ?>>
+							<?php echo $topic['name']; ?>
+						</option>
+					<?php endforeach ?>
+				</select>
+
+				<?php if ($isEditingPost === true): ?> 
+					<button type="submit" class="btn" name="update_post">UPDATE</button>
+				<?php else: ?>
+					<button type="submit" class="btn" name="create_post">Save Post</button>
+				<?php endif ?>
+			</form>
+		</div>
+	</div>
+</body>
+</html>
+
+<script src="https://cdn.ckeditor.com/4.16.2/standard/ckeditor.js"></script>
+<script> CKEDITOR.replace('body'); </script>
---
+++ web/web_backup/weblog_V0/includes/admin_functions.php
@@ -0,0 +1,149 @@
+<?php
+include('../config.php');
+include('config.php'); 
+
+
+$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+if (isset($_POST["create_admin"])) {
+    $username = $_POST['username'] ?? '';
+    $email = $_POST['email'] ?? '';
+    $role = $_POST['role_id'] ?? '';
+    $password = $_POST['password'] ?? '';
+    $password_confirmation = $_POST['passwordConfirmation'] ?? '';
+
+    if ($username && $email && $role && $password && ($password==$password_confirmation)) {
+        $hashedPassword = md5($password);
+
+        $now = date('Y-m-d H:i:s');
+
+        $stmt = $conn->prepare("INSERT INTO users (username, email, role, password, created_at, updated_at) 
+                                VALUES (?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param("ssssss", $username, $email, $role, $hashedPassword, $now, $now);
+
+        if ($stmt->execute()) {
+            $_SESSION["message"] = "Utilisateur enregistrer avec succès";
+        } else {
+            $_SESSION["message"] = "Erreur lors durant enregistrement :   '$stmt->error'";
+        }
+
+        $stmt->close();
+    } else {
+        $_SESSION["message"] = "Tous les champs sont requis.'$username,$email,$role,$password,$password_confirmation'";
+    }
+}
+
+if (isset($_GET["edit-admin"])) {
+    $id = $_GET['edit-admin'] ?? '';
+    $email = $_POST['email'] ?? '';
+    $role = $_POST['role_id'] ?? '';
+    $password = $_POST['password'] ?? '';
+    $password_confirmation = $_POST['passwordConfirmation'] ?? '';
+
+    if ($username && $email && $role && $password && ($password==$password_confirmation)) {
+        $hashedPassword = md5($password);
+
+        $now = date('Y-m-d H:i:s');
+
+        $stmt = $conn->prepare("INSERT INTO users (username, email, role, password, created_at, updated_at) 
+                                VALUES (?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param("ssssss", $username, $email, $role, $hashedPassword, $now, $now);
+
+        if ($stmt->execute()) {
+            $_SESSION["message"] = "Utilisateur enregistrer avec succès";
+        } else {
+            $_SESSION["message"] = "Erreur lors durant enregistrement :   '$stmt->error'";
+        }
+
+        $stmt->close();
+    } else {
+        $_SESSION["message"] = "Tous les champs sont requis.'$username,$email,$role,$password,$password_confirmation'";
+    }
+}
+
+if (isset($_GET["delete-admin"])){
+    deleteUser($_GET["delete-admin"]);
+}
+
+if (isset($_GET["delete-topic"])){
+    deleteTopic($_GET["delete-topic"]);
+}
+
+function deleteTopic($id){
+    $sql = "DELETE FROM topics WHERE id='$id'";
+    $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    $result = $conn->query($sql);
+    if ($result){
+        return TRUE;
+    }
+    return FALSE;
+    
+}
+
+function getAdminRoles(){
+    $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    $sql = "SELECT name FROM roles";
+    $result = $conn->query($sql);
+    $roles=[];
+    while ($result_value = $result->fetch_assoc()){
+        $roles[]=$result_value["name"];
+    }
+    return $roles;
+
+}
+
+function getAdminUsers(){
+        $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    $sql = "SELECT 
+    users.* 
+    FROM users
+    LEFT JOIN roles ON roles.name = users.role
+    ORDER BY users.created_at ASC";
+
+    $result = $conn->query($sql);
+    return ($result);
+}
+
+function getNumberUsers(){
+    $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    $sql = "SELECT COUNT(username) AS N_users FROM users";
+    $result = $conn->query($sql);
+    $N_users = ($result->fetch_assoc())["N_users"];
+    return $N_users;
+}
+
+function getNumberPosts(){
+    $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    $sql = "SELECT COUNT(id) AS N_posts FROM posts";
+    $result = $conn->query($sql);
+    $N_posts = ($result->fetch_assoc())["N_posts"];
+    return $N_posts;
+}
+
+function getNumberComments(){
+    $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    $sql = "SELECT COUNT(id) AS N_comments FROM posts";
+    $result = $conn->query($sql);
+    $N_comments = ($result->fetch_assoc())["N_comments"];
+    return $N_comments;
+}
+
+function deleteUser($User_id){
+    $sql="DELETE FROM users WHERE id='$User_id'";
+    $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    $result = $conn->query($sql);
+    if ($result){
+        return TRUE;
+    }
+    return FALSE;
+}
+
+function getTopics(){
+    $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    $sql = "SELECT * FROM topics";
+    $result = $conn->query($sql);
+    return $result;
+}
+
+
+?>
