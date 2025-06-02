4b433d3a89958dfc4fe87f99579bfd6c139639c7
T.Ligniere <45320522+TLigniere@users.noreply.github.com>
2025-06-02 21:58:59 +0200

    Update navbar.php

--- web/weblog_V0/includes/public/navbar.php
+++ web/weblog_V0/includes/public/navbar.php
@@ -1,11 +1,14 @@
 <div class="navbar">
-	<div class="logo_div">
-		<h1>MyWebSite</h1>
-	</div>
-	<ul>
-	  <li><a class="active" href="index.php">Home</a></li>
-	  <li><a href="#news">News</a></li>
-	  <li><a href="#contact">Contact</a></li>
-	  <li><a href="#about">About</a></li>
-	</ul>
+        <div class="logo_div">
+                <h1>MyWebSite</h1>
+        </div>
+        <ul>
+          <li><a class="active" href="index.php">Home</a></li>
+          <li><a href="<?php echo 'http://localhost/news.php'; ?>">News</a></li>
+          <li><a href="<?php echo 'http://localhost/contact.php'; ?>">Contact</a></li>
+          <li><a href="<?php echo 'http://localhost/about.php'; ?>">About</a></li>
+        </ul>
 </div>
+
+
+
